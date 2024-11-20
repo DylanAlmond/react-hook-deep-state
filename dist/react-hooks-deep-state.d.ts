c0a1e2301e9102bb992abe4e3958d6d9844a167d
Dylan Almond <dylan@dylanalmond.net>
2024-11-20 09:26:04 +0000

    remove ignored files

--- dist/react-hooks-deep-state.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * A custom hook for managing deeply nested state objects.
- * @param initialState - The initial state object.
- * @returns An array containing the current state and a function to update it.
- */
-export declare function useDeepState<T extends Record<string, any>>(initialState?: T): readonly [T, (path: string, value: any, merge?: boolean) => void];
-
-export { }
