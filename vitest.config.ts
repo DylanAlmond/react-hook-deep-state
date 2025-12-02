b57ae5fa6671fc37eb9f7b7ffd67a5b786ba2d4c
Dylan Almond <dylan@dylanalmond.net>
2025-12-02 19:23:16 +0000

    add vitest coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,14 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    environment: 'jsdom'
+    environment: 'jsdom',
+    globals: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['html', 'json-summary', 'json'],
+      reportOnFailure: true,
+      enabled: true
+    }
   },
   resolve: {
     // Solves ESM import errors
